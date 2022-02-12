28322d8a514b04ba4d0c4370ed3b3d28bbff2a13
me-ro-na <miracle0200@gmail.com>
2022-02-13 01:30:58 +0900

    0213_commit_hw

---
+++ __pycache__/static/assets/js/init/chatbot-init.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".select2_el").select2({});
+    //Toggle fullscreen
+    $(".chat-bot-icon").click(function(e){
+        console.log("glglgl")
+        $(this).children('img').toggleClass('hide');
+        $(this).children('svg').toggleClass('animate');
+        $('.chat-screen').toggleClass('show-chat');
+    });
+    $('.chat-mail button').click(function(){
+        $('.chat-mail').addClass('hide');
+        $('.chat-body').removeClass('hide');
+        $('.chat-input').removeClass('hide');
+        $('.chat-header-option').removeClass('hide');
+    });
+    $('.end-chat').click(function(){
+        $('.chat-body').addClass('hide');
+        $('.chat-input').addClass('hide');
+        $('.chat-session-end').removeClass('hide');
+        $('.chat-header-option').addClass('hide');
+    });
+});
